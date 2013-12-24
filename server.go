dd952f7918254981f6260fac69f1c31634edc7e1
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2013-12-24 08:53:25 +0530

    Added request options testing

--- server.go
+++ server.go
@@ -3,16 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/codegangsta/martini"
+	"github.com/wsxiaoys/terminal"
 	"net/http"
 )
-
-func createServer() *martini.ClassicMartini {
-	r := martini.NewRouter()
-	m := martini.New()
-	m.Use(martini.Recovery())
-	m.Action(r.Handle)
-	return &martini.ClassicMartini{m, r}
-}
 
 func main() {
 	app := createServer()
@@ -20,9 +13,9 @@ func main() {
 
 	app.Get("/v1/", func(rw http.ResponseWriter, req *http.Request) {
 		Suite("Client Options")
-
 		Test("Base value given by api")
 		Test("Api version value given by api")
+		Test("Suffix defined by api")
 
 		if req.UserAgent() == "alpaca/0.1.0 (https://github.com/pksunkara/alpaca)" {
 			Test("User agent value given by api")
@@ -32,15 +25,45 @@ func main() {
 	})
 
 	app.Get("/v1/method", func() string {
-		Suite("Methods")
+		Suite("HTTP Methods")
 		Test("GET request")
-
 		return "/"
 	})
 
 	app.Post("/v1/method", func() string {
 		Test("POST request")
 		return "/"
+	})
+
+	app.Put("/v1/method", func() string {
+		Test("PUT request")
+		return "/"
+	})
+
+	app.Patch("/v1/method", func() string {
+		Test("PATCH request")
+		return "/"
+	})
+
+	app.Delete("/v1/method", func() string {
+		Test("DELETE request")
+		return "/"
+	})
+
+	app.Get("/v2/", func(rw http.ResponseWriter, req *http.Request) {
+		Suite("Request Options")
+		Test("Base value should not be changed")
+		Test("Api version value given by options")
+
+		if req.UserAgent() == "alpaca/0.1.0 (https://github.com/pksunkara/alpaca)" {
+			Test("User agent value should not be changed")
+		}
+
+		if req.Header["Custom-Header"][0] == "custom" {
+			Test("Headers value given by options")
+		}
+
+		fmt.Fprintf(rw, "/")
 	})
 
 	tmp.Get("/v2/", func(rw http.ResponseWriter, req *http.Request) {
@@ -55,9 +78,27 @@ func main() {
 			Test("Headers value given by options")
 		}
 
+		Test("Base value's path should not be used")
+
 		fmt.Fprintf(rw, "/")
 	})
 
 	go http.ListenAndServe(":3000", app)
 	http.ListenAndServe(":3001", tmp)
+}
+
+func Suite(name string) {
+	terminal.Stdout.Nl().Color("y!").Print("• ").Color("w!").Print(name).Nl()
+}
+
+func Test(name string) {
+	terminal.Stdout.Color("g!").Print("\t✓ ").Color("|").Print(name).Nl()
+}
+
+func createServer() *martini.ClassicMartini {
+	r := martini.NewRouter()
+	m := martini.New()
+	m.Use(martini.Recovery())
+	m.Action(r.Handle)
+	return &martini.ClassicMartini{m, r}
 }
--- test.rb
+++ test.rb
@@ -3,7 +3,7 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 require "test-alpaca"
 
-client = Test::Client.new
+client = Test::Client.new({}, { :suffix => false })
 client.client_options.base
 
 Test::Client.new({}, {
@@ -11,9 +11,22 @@ Test::Client.new({}, {
   :api_version => 'v2',
   :user_agent => 'testing (user agent)',
   :headers => {
-    "custom-header" => 'custom'
-  }
+    'custom-header' => 'custom'
+  },
+  :suffix => false
 }).client_options.base
+
+client.request_options.base({
+  :base => 'http://localhost:3001/useless',
+  :api_version => 'v2',
+  :user_agent => 'testing (user agent)',
+  :headers => {
+    'custom-header' => 'custom'
+  }
+})
 
 client.methods.get
 client.methods.post
+client.methods.put
+client.methods.patch
+client.methods.delete
