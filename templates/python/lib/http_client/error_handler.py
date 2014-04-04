5ebc2abb25462c719c2f2065c241848ae57079df
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-04-04 15:01:32 +0530

    Fix python's indentation

--- templates/python/lib/http_client/error_handler.py
+++ templates/python/lib/http_client/error_handler.py
@@ -4,29 +4,29 @@ from .response_handler import ResponseHandler
 # ErrorHanlder takes care of selecting the error message from response body
 class ErrorHandler():
 
-	@staticmethod
-	def check_error(response, *args, **kwargs):
-		code = response.status_code
-		typ = response.headers.get('content-type')
+    @staticmethod
+    def check_error(response, *args, **kwargs):
+        code = response.status_code
+        typ = response.headers.get('content-type')
 
-		if code in range(500, 600):
-			raise ClientError('Error ' + str(code), code)
-		elif code in range(400, 500):
-			body = ResponseHandler.get_body(response)
-			message = ''
+        if code in range(500, 600):
+            raise ClientError('Error ' + str(code), code)
+        elif code in range(400, 500):
+            body = ResponseHandler.get_body(response)
+            message = ''
 
-			# If HTML, whole body is taken
-			if isinstance(body, str):
-				message = body
+            # If HTML, whole body is taken
+            if isinstance(body, str):
+                message = body
 {{if .Api.response.formats.json}}
-			# If JSON, a particular field is taken and used
-			if typ.find('json') != -1 and isinstance(body, dict):
-				if '{{.Api.error.message}}' in body:
-					message = body['{{.Api.error.message}}']
-				else:
-					message = 'Unable to select error message from json returned by request responsible for error'
+            # If JSON, a particular field is taken and used
+            if typ.find('json') != -1 and isinstance(body, dict):
+                if '{{.Api.error.message}}' in body:
+                    message = body['{{.Api.error.message}}']
+                else:
+                    message = 'Unable to select error message from json returned by request responsible for error'
 {{end}}
-			if message == '':
-				message = 'Unable to understand the content type of response returned by request responsible for error'
+            if message == '':
+                message = 'Unable to understand the content type of response returned by request responsible for error'
 
-			raise ClientError(message, code)
+            raise ClientError(message, code)
--- templates/python/lib/http_client/response.py
+++ templates/python/lib/http_client/response.py
@@ -1,6 +1,7 @@
 # Response object contains the response returned by the client
 class Response():
-	def __init__(self, body, code, headers):
-		self.body = body
-		self.code = code
-		self.headers = headers
+
+    def __init__(self, body, code, headers):
+        self.body = body
+        self.code = code
+        self.headers = headers
