17080b485e58dc01ba55c7e94ecf97ea60692075
Harald Nordgren <haraldnordgren@gmail.com>
2018-10-28 10:55:36 +0100

    Fix lint errors

--- alpaca/error.go
+++ alpaca/error.go
@@ -7,7 +7,8 @@ import (
 
 func HandleError(e error) {
 	if e != nil {
-		fmt.Println("Encountered an error while running!\n")
+		fmt.Println("Encountered an error while running!")
+		fmt.Println()
 		fmt.Println(e)
 		fmt.Println("\nIf you are unable to solve it, please report at")
 		fmt.Println("https://github.com/pksunkara/alpaca/issues")
